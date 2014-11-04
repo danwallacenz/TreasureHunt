f6a296b1d1645817ebe67bfff2905b760240275d
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-11-04 18:42:40 +1300

    add subclasses of Treasure

---
+++ GeoLocation.swift
@@ -0,0 +1,15 @@
+//
+//  GeoLocation.swift
+//  TreasureHunt
+//
+//  Created by Daniel Wallace on 4/11/14.
+//  Copyright (c) 2014 Razeware. All rights reserved.
+//
+
+import Foundation
+
+
+struct GeoLocation {
+    var latitude: Double
+    var longitude: Double
+}
